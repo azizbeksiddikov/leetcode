3a6336f85c432ef8d190928cbb583443fc55ae59
Azizbek Siddikov <azbeksid@gmail.com>
2025-09-15 23:58:05 +0900

    feat: implement twoSum function

---
+++ [1]-two-sum.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {} # how much is req : index of other value
+
+        for i in range(len(nums)):
+            curr_value = nums[i]
+            required = target - curr_value
+
+            if required in seen:
+                return [seen[required], i]    
+            else:
+                seen[curr_value] = i
