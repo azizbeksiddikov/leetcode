eece7a186a522c521475b4c73283907638f13c97
Azizbek Siddikov <azbeksid@gmail.com>
2026-03-22 11:43:01 +0900

    feat: optimize 228

--- medium/238. Product of Array Except Self.py
+++ medium/238. Product of Array Except Self.py
@@ -2,39 +2,23 @@
 
 class Solution:
     def productExceptSelf(self, nums: list[int]) -> list[int]:
-        prefix_arr: list[int] = []
-        postfix_arr = [1] * len(nums)
-        result = [1] * len(nums)
+        n = len(nums)
+        answer = [1] * n
         
-        # original:[1,  2,   4, 6]
-        # prefix:  [1,  1,   2, 8]
-        # postfix: [48, 24,  6, 1]
-        # result:  [48, 24, 12, 8]
+        # Calculate Prefix products
+        prefix = 1
+        for i in range(n):
+            answer[i] = prefix
+            prefix *= nums[i]
         
-        print("nums", nums)
+        # Calculate Suffix products
+        suffix = 1
+        for i in range(n-1, -1, -1):
+            answer[i] *= suffix
+            suffix *= nums[i]
         
-        # start => end: prefix arr
-        for idx, num in enumerate(nums):
-            # print(idx, num)
-            if idx == 0:
-                prefix_arr.append(1)
-                continue
-            prefix_arr.append(prefix_arr[idx-1] * nums[idx-1])
-        print("prefix arr", prefix_arr)
-        
-        # end => start: postfix arr
-        for idx in range(len(nums)-1, -1, -1):
-            if idx == len(nums)-1:
-                postfix_arr[idx] = 1
-                continue
-            postfix_arr[idx] = nums[idx+1] * postfix_arr[idx+1]
-        print("postfix_arr", postfix_arr)
-        
-        result = [postfix_arr[idx] * prefix_arr[idx] for idx in range(len(nums))]
-        return result
+        return answer
 
-
-
-# nums = [1,2,4,6]
+# nums = [1,2,3,4]
 # result = Solution().productExceptSelf(nums)
 # print("result", result)
