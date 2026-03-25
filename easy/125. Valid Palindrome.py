fdc690880f866ce312c8e8d70806c3886269830c
Azizbek Siddikov <azbeksid@gmail.com>
2026-03-25 22:44:39 +0900

    feat: optimize 125

--- easy/125. Valid Palindrome.py
+++ easy/125. Valid Palindrome.py
@@ -3,18 +3,8 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         new_s = "".join(char.lower() for char in s if char.isalnum())
+        return new_s==new_s[::-1] 
         
-        
-        start = 0
-        end = -1
-        
-        for i in range(len(new_s)):
-            if new_s[start] != new_s[end]:
-                return False
-            start += 1
-            end -= 1
-        
-        return True
     
 # s = "Was it a car or a cat I saw?"
 s = "race a car"
