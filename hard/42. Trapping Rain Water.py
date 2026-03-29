2d8f53c4a245d03013e4d63cc8c0ca600b071d35
Azizbek Siddikov <azbeksid@gmail.com>
2026-03-29 14:41:47 +0900

    feat: improve 42 to const space complexity

--- hard/42. Trapping Rain Water.py
+++ hard/42. Trapping Rain Water.py
@@ -2,32 +2,22 @@
 
 class Solution:
     def trap(self, height: list[int]) -> int:
-        n = len(height)
-        res = 0
-        max_heights = [0] * n
+        l, r = 0, len(height) - 1
+        l_max, r_max = height[l], height[r]
+        total_water = 0
         
-        # Find the left max
-        l_max = 0
-        for i, a in enumerate(height):
-            if a > height[l_max]:
-                l_max = i
-            max_heights[i] = height[l_max]
-        # print("after left max iterations:", max_heights)
-
-        # Find the right max - reverser order
-        r_max = n - 1
-        for i in range(n - 1, -1, -1):
-            if height[i] > height[r_max]:
-                r_max = i
-            max_heights[i] = min(max_heights[i], height[r_max])
-        # print("after right max iterations:", max_heights)
-        
-        # Calculate the area
-        for i, a in enumerate(height):
-            res += max_heights[i] - a
-            # print(f"{i} index: height={max_heights[i]}, water: {max_heights[i] - a}")
-        return res
+        while l < r:
+            # print(f"Compare: l={l} {height[l]}; r={r} {height[r]}; l_max={l_max}, r_max={r_max}")
+            if l_max <= r_max:
+                l += 1
+                l_max = max(l_max, height[l])
+                total_water += l_max - height[l]
+            else:
+                r -= 1
+                r_max = max(r_max, height[r])
+                total_water += r_max - height[r]
+            # print(f"total_water: {total_water}")
+        return total_water
         
 # height = [0,2,0,3,1,0,1,3,2,1]
-# print("res:", Solution().trap(height))
-
+# print("total_water:", Solution().trap(height))
