87cb35a7415270da2927cb18ecf4b1c694c5181d
Azizbek Siddikov <azbeksid@gmail.com>
2026-04-14 08:16:47 +0900

    feat: improve 424

--- medium/424. Longest Repeating Character Replacement.py
+++ medium/424. Longest Repeating Character Replacement.py
@@ -2,30 +2,19 @@
 
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        maxLength = 0
-        l, r = 0, 0
+        freq = {}
+        maxFreq, res = 0, 0
+        l = 0
         
-        def countDict(dict):
-            # return (all - mostFreq char count)
-            array = [dict[ele] for ele in dict]
-            if not array:
-                return 0
-            return sum(array) - max(array)
-         
-        charDict = {}
-        while r < len(s):
-            # print("before:", l, r, maxLength, charDict, countDict(charDict))
-            
-            charDict[s[r]] = charDict.get(s[r], 0) + 1
-            while countDict(charDict) > k:
-                charDict[s[l]] -= 1
-                l += 1
-            maxLength = max(maxLength, (r - l + 1))
-            
-            # print("after: ", l, r, maxLength, charDict, countDict(charDict), "\n")
-            r += 1
+        for r in range(len(s)):
+            freq[s[r]] = freq.get(s[r], 0) + 1
 
-        return maxLength
+            maxFreq = max(maxFreq, freq[s[r]])
+            while (r - l + 1) - maxFreq > k:
+                freq[s[l]] -= 1
+                l += 1
+            res = max(res, r - l + 1)
+        return res
         
 # s, k = "XYYX", 2 # 4
 # s, k = "AAABABB", 1 # 5
