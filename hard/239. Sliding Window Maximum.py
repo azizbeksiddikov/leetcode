878d068d9be7824449a74ed067b8cda336c0618d
Azizbek Siddikov <azbeksid@gmail.com>
2026-04-26 14:55:59 +0900

    feat: improve 239

--- hard/239. Sliding Window Maximum.py
+++ hard/239. Sliding Window Maximum.py
@@ -1,31 +1,24 @@
 # Link: https://leetcode.com/problems/sliding-window-maximum
+from collections import deque
 
 class Solution:
     def maxSlidingWindow(self, nums: list[int], k: int) -> list[int]:
         res = []
-        queue = []
+        queue = deque()
+        l = r = 0
 
-        for i in range(len(nums) - k + 1):
-            print(f"start: {nums[i : i + k]}, queue: {queue}, res: {res}")
-            
-            # first built-up
-            if i == 0:
-                for idx in range(k):
-                    while queue and nums[queue[-1]] < nums[idx]:
-                        queue.pop()
-                    queue.append(idx)
-            else:
-                newNumberIndex = i + k - 1
-                
-                while queue and nums[queue[-1]] < nums[newNumberIndex]:
-                    queue.pop()
-                queue.append(newNumberIndex)
+        while r < len(nums):
+            while queue and nums[queue[-1]] < nums[r]:
+                queue.pop()
+            queue.append(r)
 
-                if queue and queue[0] < i:
-                    del queue[0]
+            if l > queue[0]:
+                queue.popleft()
             
-            res.append(nums[queue[0]])
-            print(f"after: {nums[i : i + k]}, queue: {queue}, res: {res}\n")
+            if (r + 1) >= k:
+                res.append(nums[queue[0]])
+                l += 1
+            r += 1
         return res
 
 # nums, k = [1,2,1,0,4,2,6], 3
